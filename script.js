557b4b3b5b7655524aa89f384825c017bb44b5fe
Valeriya <106891874+V-Valkiriya@users.noreply.github.com>
2023-06-27 19:31:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17979,7 +17979,8 @@ var modals = function modals() {
   bindModal('.phone_link', '.popup', '.popup .popup_close');
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
   bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
-  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false); // showModalByTime('.popup', 6000);
+  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false); 
+  showModalByTime('.popup', 6000);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (modals);
